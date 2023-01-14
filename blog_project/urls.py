15e6b56995fb9c9fcb19cb4930f2481bde39ba67
admin <nailfax1991@yandex.ru>
2023-01-14 17:12:25 +0200

    forms and user accounts

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), # Добавили новый маршрут
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), # новое добавление
     path('', include('blog.urls')),
 ]
